95963eb048291d6f0c5e44f8b2a403b675fa2ae3
faizalpribadi <faizal.pribadi@gmail.com>
2015-12-22 21:47:38 +0700

    moved to test directory

--- test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-var assert = require('assert');
-var processExit = require('./');
-
-it('should ', function () {
-	assert.strictEqual(processExit('unicorns'), 'unicorns & rainbows');
-});
