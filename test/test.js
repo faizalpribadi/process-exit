4bf06da6a4b26a20d5720045a6ae4e9603c1973a
faizalpribadi <faizal.pribadi@gmail.com>
2015-12-22 21:47:15 +0700

    test with mocha & expect.js

---
+++ test/test.js
@@ -0,0 +1,8 @@
+'use strict';
+var processExit = require('../');
+
+describe('Should be test of method on process-exit', function() {
+	it('has own property if available in the method', function(next) {
+		// do something
+	});
+});
